eceddc1c725ef84d03eaafd89d7be944e80d64b3
Kimo0104 <55680839+Kimo0104@users.noreply.github.com>
2022-09-11 09:50:14 -0600

    aa

--- src/components/calendar/calendar.js
+++ src/components/calendar/calendar.js
@@ -53,6 +53,7 @@ const Calendar = ({data}) => {
     return <div id = "calendar" className = 'calendar'>
         <Scheduler
             data = {state.appointments}
+            height = {'auto'}
         >
             <ViewState currentDate = {correctSunday}/>
             <EditingState onCommitChanges = {commitChanges}/>
